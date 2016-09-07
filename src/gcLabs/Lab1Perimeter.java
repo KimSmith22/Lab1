92fdb6c44ed0a378c42800f60d03a25a0768de32
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-07 16:22:40 -0400

    Initial commit

---
+++ src/gcLabs/Lab1Perimeter.java
@@ -0,0 +1,47 @@
+package gcLabs;
+
+// import java scanner
+import java.util.Scanner;
+
+//declare public class
+public class Lab1Perimeter {
+
+		private static Scanner k;
+
+		//declare method
+		public static void main (String[] args) {
+			//declare variables
+			double length;
+			double width;
+			
+			String choice;
+			choice = "y";
+			
+			
+			//system output and user prompts
+			System.out.println("Welcome to Grand Circus' Room Detail Generator!");
+			
+		//do the following:
+			do {
+			System.out.print("Enter Length:  ");
+			k = new Scanner(System.in);
+			length = k.nextDouble();
+			System.out.print("Enter Width:  ");
+			width = k.nextDouble();
+			
+			//calculate and output area and perimeter
+			System.out.println("Area:  " + (length * width));
+			System.out.println("Perimeter:  " + (2 * width + 2 * length));	
+			
+			//blank space and prompt for continue
+			System.out.println();
+			System.out.print("Continue? (y/n): ");
+			k.nextLine();
+			/*this following tells the program to 
+			 * store the information input by the user
+			 */
+			choice = k.nextLine();
+			// as long as the user answers yes
+			}while (choice.equals("y"));
+		}
+}
